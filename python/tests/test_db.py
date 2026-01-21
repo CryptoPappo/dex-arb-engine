c4c95f64a26c8a9a5f3787c9f0179e4d99ac7006
Juan Cruz Pappolla <jcp12196@gmail.com>
2026-01-21 18:29:40 -0300

    Added dex test

--- python/tests/test_db.py
+++ python/tests/test_db.py
@@ -9,8 +9,8 @@ parent_dir = os.path.abspath(os.path.join(current_dir, ".."))
 sys.path.append(parent_dir)
 
 from db.connection import create_connection
-from db.models import Base, Chains
-from db.populate_tables import populate_chains
+from db.models import Base, Chains, Dex
+from db.populate_tables import populate_chains, populate_dex
 
 def test_tables_created():
     engine = create_connection("sqlite:///:memory:")
@@ -37,3 +37,18 @@ def test_populate_chains():
         assert eth.name == "Ethereum"
         assert eth.native_token == "ETH"
         assert eth.evm_compatible == True
+
+def test_populate_dex():
+    engine = create_connection("sqlite:///:memory:")
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    populate_dex(Session)
+
+    with Session() as session:
+        dex = session.get(Dex, 1)
+        assert dex.chain_id == 1
+        assert dex.name == "Uniswap"
+        assert dex.dex_type == "V2"
+        assert dex.factory_address == "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f"
+        assert dex.quoter_address == "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"
