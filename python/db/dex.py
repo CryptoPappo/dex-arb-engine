554dd8e86963de71d5380c67220cd15114ff9ea1
Juan Cruz Pappolla <jcp12196@gmail.com>
2026-02-04 15:33:05 -0300

    Added V3 and V4 adapters

--- python/db/dex.py
+++ python/db/dex.py
@@ -2,10 +2,18 @@ from typing import Optional, Iterator, Tuple, Any
 from abc import ABC, abstractmethod
 from web3 import Web3
 from web3.contract.contract import Contract
-from eth_abi import abi
+import eth_abi as eth
+import eth_hash.auto as ethash 
+
 from db.models import Tokens, Pools
 
 ZERO_ADDRESS = "0x0000000000000000000000000000000000000000"
+TICK_SPACING = {
+        100: 1,
+        500: 10,
+        3000: 60,
+        10000: 200,
+}
 
 def decode_data(
         function_names: list[str],
@@ -24,7 +32,7 @@ def decode_data(
                 if name == function_name:
                     function_types = [outputs["internalType"] for outputs in item["outputs"]]
         if function_types:
-            response.append(abi.decode(function_types, data))
+            response.append(eth.abi.decode(function_types, data))
     return response
 
 def token_pair_fee_iter(
@@ -73,29 +81,29 @@ class UniswapV2Adapter(DexAdapter):
         self.dex_id = dex_id
 
     def _pool_combinations(self) ->  Iterator[Tuple[Tokens, Tokens, int | None]]:
-        yield from token_pair_fee_iter(self.tokens)
+        yield from token_pair_fee_iter(self.tokens, [3000])
 
     def fetch_pools(self) -> list[Pools]:
         calls = []
         possible_pools = []
-        for token0, token1, _ in self._pool_combinations():
+        for token0, token1, fee in self._pool_combinations():
             calls.append(
                 (
                     self.factory_contract.address,
                     True,
                     self.factory_contract.functions
-                    .getPair(token0, token1)
+                    .getPair(token0.address, token1.address)
                     ._encode_transaction_data(),
                 )
             )
             possible_pools.append(
                     Pools(
-                        chain_id = self.chain_id,
-                        dex_id = self.dex_id,
-                        pool_address = "",
-                        token0 = token0.address,
-                        token1 = token1.address,
-                        fee = 3000,
+                        chain_id=self.chain_id,
+                        dex_id=self.dex_id,
+                        pool_address="",
+                        token0=token0.address,
+                        token1=token1.address,
+                        fee=fee,
                     )
             )
         data_encoded = self.multicall_contract.functions.aggregate3(calls).call()
@@ -111,6 +119,132 @@ class UniswapV2Adapter(DexAdapter):
 
             if pool_address != ZERO_ADDRESS:
                 possible_pools[index].pool_address = pool_address
+                pools.append(possible_pools[index])
+
+        return pools
+
+class UniswapV3Adapter(DexAdapter):
+
+    def __init__(
+            self,
+            factory_contract: Contract, 
+            multicall_contract: Contract,
+            tokens: list[Tokens],
+            chain_id: int,
+            dex_id: int
+    ):   
+        self.factory_contract = factory_contract
+        self.multicall_contract = multicall_contract
+        self.tokens = tokens
+        self.chain_id = chain_id
+        self.dex_id = dex_id
+
+    def _pool_combinations(self) ->  Iterator[Tuple[Tokens, Tokens, int | None]]:
+        yield from token_pair_fee_iter(self.tokens, [100, 500, 3000, 10000])
+
+    def fetch_pools(self) -> list[Pools]:
+        calls = []
+        possible_pools = []
+        for token0, token1, fee in self._pool_combinations():
+            calls.append(
+                (
+                    self.factory_contract.address,
+                    True,
+                    self.factory_contract.functions
+                    .getPool(token0.address, token1.address, fee)
+                    ._encode_transaction_data(),
+                )
+            )
+            possible_pools.append(
+                    Pools(
+                        chain_id=self.chain_id,
+                        dex_id=self.dex_id,
+                        pool_address="",
+                        token0=token0.address,
+                        token1=token1.address,
+                        fee=fee,
+                        tick_spacing=TICK_SPACING[fee],
+                    )
+            )
+        data_encoded = self.multicall_contract.functions.aggregate3(calls).call()
+        
+        pools = []
+        for index, data in enumerate(data_encoded):
+            pool_data = decode_data(
+                    ["getPool"],
+                    [data[1]],
+                    [self.factory_contract.abi]
+            )
+            pool_address = Web3.to_checksum_address(pool_data[0][0])
+
+            if pool_address != ZERO_ADDRESS:
+                possible_pools[index].pool_address = pool_address
+                pools.append(possible_pools[index])
+
+        return pools
+
+class UniswapV4Adapter(DexAdapter):
+
+    def __init__(
+            self,
+            factory_contract: Contract, 
+            multicall_contract: Contract,
+            tokens: list[Tokens],
+            chain_id: int,
+            dex_id: int
+    ):   
+        self.factory_contract = factory_contract
+        self.multicall_contract = multicall_contract
+        self.tokens = tokens
+        self.chain_id = chain_id
+        self.dex_id = dex_id
+
+    def _pool_combinations(self) ->  Iterator[Tuple[Tokens, Tokens, int | None]]:
+        yield from token_pair_fee_iter(self.tokens, [100, 500, 3000, 10000])
+
+    def fetch_pools(self) -> list[Pools]:
+        calls = []
+        possible_pools = []
+        for token0, token1, fee in self._pool_combinations():
+            tick_spacing = TICK_SPACING[fee]
+            pool_id = "0x" + ethash.keccak(
+                    eth.encode(
+                        ["address", "address", "uint24", "int24", "address"],
+                        [token0.address, token1.address, fee, tick_spacing, ZERO_ADDRESS]
+                    )
+            ).hex()
+            calls.append(
+                (
+                    self.factory_contract.address,
+                    True,
+                    self.factory_contract.functions
+                    .getLiquidity(pool_id)
+                    ._encode_transaction_data(),
+                )
+            )
+            possible_pools.append(
+                    Pools(
+                        chain_id=self.chain_id,
+                        dex_id=self.dex_id,
+                        pool_address=pool_id,
+                        token0=token0.address,
+                        token1=token1.address,
+                        fee=fee,
+                        tick_spacing=tick_spacing,
+                    )
+            )
+        data_encoded = self.multicall_contract.functions.aggregate3(calls).call()
+        
+        pools = []
+        for index, data in enumerate(data_encoded):
+            pool_data = decode_data(
+                    ["getLiquidity"],
+                    [data[1]],
+                    [self.factory_contract.abi]
+            )
+            liquidity = pool_data[0][0]
+
+            if liquidity != 0:
                 pools.append(possible_pools[index])
 
         return pools
--- python/tests/test_dex.py
+++ python/tests/test_dex.py
@@ -2,14 +2,15 @@ import pytest
 import os
 import sys
 from unittest.mock import MagicMock
-from eth_abi import encode
+import eth_abi as eth
+import eth_hash.auto as ethash
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.abspath(os.path.join(current_dir, ".."))
 sys.path.append(parent_dir)
 
 from db.models import Tokens
-from db.dex import UniswapV2Adapter
+from db.dex import UniswapV2Adapter, UniswapV3Adapter, UniswapV4Adapter
 
 ZERO_ADDRESS = "0x0000000000000000000000000000000000000000"
 
@@ -28,9 +29,9 @@ def make_mock_factory_v2(return_map):
     }
 
     def get_pair(token0, token1):
-        pair_address = return_map.get((token0.symbol, token1.symbol), ZERO_ADDRESS)
+        pair_address = return_map.get((token0, token1), ZERO_ADDRESS)
         encoded_data = MagicMock()
-        encoded_data._encode_transaction_data.return_value = pair_address
+        encoded_data._encode_transaction_data.return_value = eth.encode(["address"], [pair_address])
 
         return encoded_data
 
@@ -38,13 +39,63 @@ def make_mock_factory_v2(return_map):
     
     return factory
 
+def make_mock_factory_v3(return_map):
+    factory = MagicMock()
+    factory.address = ""
+    factory.abi = {
+            "name": "getPool",
+            "outputs": [
+                {
+                    "internalType":"address",
+                    "name":"",
+                    "type":"address"
+                 }
+            ]
+    }
+
+    def get_pool(token0, token1, fee):
+        pair_address = return_map.get((token0, token1, fee), ZERO_ADDRESS)
+        encoded_data = MagicMock()
+        encoded_data._encode_transaction_data.return_value = eth.encode(["address"], [pair_address])
+
+        return encoded_data
+
+    factory.functions.getPool.side_effect = get_pool
+    
+    return factory
+
+def make_mock_factory_v4(return_map):
+    factory = MagicMock()
+    factory.address = ""
+    factory.abi = {
+            "name": "getLiquidity",
+            "outputs": [
+                {
+                    "internalType":"uint128",
+                    "name":"liquidity",
+                    "type":"uint128"
+                 }
+            ]
+    }
+
+    def get_liquidity(pool_id):
+        liquidity = return_map.get(pool_id, 0)
+        encoded_data = MagicMock()
+        encoded_data._encode_transaction_data.return_value = eth.encode(["uint128"], [liquidity])
+
+        return encoded_data
+
+    factory.functions.getLiquidity.side_effect = get_liquidity
+    
+    return factory
+
 def make_mock_multicall():
     multicall = MagicMock()
     
     def aggregate(calls):
-        addresses = [[True, encode(["address"], [address])] for _, _, address in calls]
+        data_encoded = [[True, data] for _, _, data in calls]
         call = MagicMock()
-        call.call.return_value = addresses
+        call.call.return_value = data_encoded
 
         return call
     
@@ -53,16 +104,16 @@ def make_mock_multicall():
     return multicall
 
 def test_uniswap_v2_adapter_filters_zero_address():
-    tokens = [
-            Tokens(coin_id=1, chain_id=1, name="", symbol="WBTC", address="0x2260fac5e5542a773aa44fbcfedf7c193bc2c599", decimals=18),
-            Tokens(coin_id=2, chain_id=1, name="", symbol="WETH", address="0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2", decimals=18),
-            Tokens(coin_id=3, chain_id=1, name="", symbol="USDC", address="0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", decimals=18),
-            Tokens(coin_id=4, chain_id=1, name="", symbol="USDT", address="0xdac17f958d2ee523a2206206994597c13d831ec7", decimals=18),
-    ]
+    wbtc = Tokens(coin_id=1, chain_id=1, name="", symbol="WBTC", address="0x2260fac5e5542a773aa44fbcfedf7c193bc2c599", decimals=18)
+    weth = Tokens(coin_id=2, chain_id=1, name="", symbol="WETH", address="0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2", decimals=18)
+    usdc = Tokens(coin_id=3, chain_id=1, name="", symbol="USDC", address="0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", decimals=18)
+    usdt = Tokens(coin_id=4, chain_id=1, name="", symbol="USDT", address="0xdac17f958d2ee523a2206206994597c13d831ec7", decimals=18)
+
+    tokens = [wbtc, weth, usdc, usdt]
     return_map = {
-            ("WBTC", "WETH"): "0xCBCdF9626bC03E24f779434178A73a0B4bad62eD",
-            ("WBTC", "USDC"): "0x99ac8cA7087fA4A2A1FB6357269965A2014ABc35",
-            ("WETH", "USDT"): "0x4e68Ccd3E89f51C3074ca5072bbAC773960dFa36",
+            (wbtc.address, weth.address): "0xCBCdF9626bC03E24f779434178A73a0B4bad62eD",
+            (wbtc.address, usdc.address): "0x99ac8cA7087fA4A2A1FB6357269965A2014ABc35",
+            (weth.address, usdt.address): "0x4e68Ccd3E89f51C3074ca5072bbAC773960dFa36",
     }
     
     factory = make_mock_factory_v2(return_map)
@@ -81,3 +132,79 @@ def test_uniswap_v2_adapter_filters_zero_address():
     assert len(pools) == 3
     assert {pool.pool_address for pool in pools} == {pool for pool in return_map.values()}
 
+def test_uniswap_v3_adapter_filters_zero_address():
+    wbtc = Tokens(coin_id=1, chain_id=1, name="", symbol="WBTC", address="0x2260fac5e5542a773aa44fbcfedf7c193bc2c599", decimals=18)
+    weth = Tokens(coin_id=2, chain_id=1, name="", symbol="WETH", address="0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2", decimals=18)
+    usdc = Tokens(coin_id=3, chain_id=1, name="", symbol="USDC", address="0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", decimals=18)
+    usdt = Tokens(coin_id=4, chain_id=1, name="", symbol="USDT", address="0xdac17f958d2ee523a2206206994597c13d831ec7", decimals=18)
+
+    tokens = [wbtc, weth, usdc, usdt]
+    return_map = {
+            (wbtc.address, weth.address, 10000): "0xCBCdF9626bC03E24f779434178A73a0B4bad62eD",
+            (wbtc.address, usdc.address, 3000): "0x99ac8cA7087fA4A2A1FB6357269965A2014ABc35",
+            (weth.address, usdt.address, 500): "0x4e68Ccd3E89f51C3074ca5072bbAC773960dFa36",
+    }
+    
+    factory = make_mock_factory_v3(return_map)
+    multicall = make_mock_multicall()
+
+    adapter = UniswapV3Adapter(
+            factory_contract=factory,
+            multicall_contract=multicall,
+            tokens=tokens,
+            chain_id=1,
+            dex_id=1,
+    )
+    
+    pools = adapter.fetch_pools()
+
+    assert len(pools) == 3
+    assert {pool.pool_address for pool in pools} == {pool for pool in return_map.values()}
+
+
+def test_uniswap_v4_adapter_filters_zero_address():
+    wbtc = Tokens(coin_id=1, chain_id=1, name="", symbol="WBTC", address="0x2260fac5e5542a773aa44fbcfedf7c193bc2c599", decimals=18)
+    weth = Tokens(coin_id=2, chain_id=1, name="", symbol="WETH", address="0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2", decimals=18)
+    usdc = Tokens(coin_id=3, chain_id=1, name="", symbol="USDC", address="0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", decimals=18)
+    usdt = Tokens(coin_id=4, chain_id=1, name="", symbol="USDT", address="0xdac17f958d2ee523a2206206994597c13d831ec7", decimals=18)
+
+    tokens = [wbtc, weth, usdc, usdt]
+    pool0 = "0x" + ethash.keccak(
+            eth.encode(
+                ["address", "address", "uint24", "int24", "address"],
+                [wbtc.address, weth.address, 10000, 200, ZERO_ADDRESS]
+                )
+    ).hex()
+    pool1 = "0x" + ethash.keccak(
+            eth.encode(
+                ["address", "address", "uint24", "int24", "address"],
+                [wbtc.address, usdc.address, 3000, 60, ZERO_ADDRESS]
+                )
+    ).hex()
+    pool2 = "0x" + ethash.keccak(
+            eth.encode(
+                ["address", "address", "uint24", "int24", "address"],
+                [weth.address, usdt.address, 500, 10, ZERO_ADDRESS]
+                )
+    ).hex() 
+    return_map = {
+            pool0: 10000,
+            pool1: 1000,
+            pool2: 10, 
+    }
+    
+    factory = make_mock_factory_v4(return_map)
+    multicall = make_mock_multicall()
+
+    adapter = UniswapV4Adapter(
+            factory_contract=factory,
+            multicall_contract=multicall,
+            tokens=tokens,
+            chain_id=1,
+            dex_id=1,
+    )
+    
+    pools = adapter.fetch_pools()
+
+    assert len(pools) == 3
+    assert {pool.pool_address for pool in pools} == {pool for pool in return_map.keys()}
