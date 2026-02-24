1a431fb894ebd83fa22e218d3b51c62da8fa2bd2
Juan Cruz Pappolla <jcp12196@gmail.com>
2026-02-24 19:11:23 -0300

    Fixed build issue

--- rust/build.rs
+++ rust/build.rs
@@ -1,5 +1,8 @@
 
 fn main() {
+    let (protoc_bin, _) = protoc_prebuilt::init("22.0").unwrap();
+    std::env::set_var("PROTOC", protoc_bin);
+
     prost_build::compile_protos(
         &["../schemas/swap_event.proto"],
         &["../schemas"],
